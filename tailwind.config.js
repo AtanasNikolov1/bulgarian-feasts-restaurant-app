c5559282dcb4797595196951e7ca2161910d4c04
AtanasNikolov1 <atanasnik989@gmail.com>
2024-07-20 23:41:49 +0300

    Customize tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customOrange: '#FF8000',
+        lightGray: '#F7F7F7',
+        lightBlack: '#353839'
+      },
+      fontSize: {
+        '2lg': '1.188rem',
+      },
+      fontFamily: {
+        main: ['Roboto', 'sans-serif'],
+      },
+      transitionDuration: {
+        '450': '450ms',
+      }
+    },
   },
   plugins: [],
 };
