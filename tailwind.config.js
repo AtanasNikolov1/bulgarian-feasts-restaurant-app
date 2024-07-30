2a137620ff14e61ec756d930afdbe6b337b3d9eb
AtanasNikolov1 <atanasnik989@gmail.com>
2024-07-30 11:56:16 +0300

    Add Tailwind custom styles and update body styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,27 @@ export default {
       colors: {
         customOrange: '#FF8000',
         lightGray: '#F7F7F7',
-        lightBlack: '#353839'
+        lightBlack: '#353839',
+        steelGray: '#37383C',
+        nightShade: '#22242A',
+        stormGray: '#6B6D70',
+        snow: '#F5F5F5',
+        utOrange: '#FF8200'
       },
       fontSize: {
         '2lg': '1.188rem',
       },
       fontFamily: {
         main: ['Roboto', 'sans-serif'],
+        headings: ['Montserrat', 'sans-serif']
       },
       transitionDuration: {
         '450': '450ms',
-      }
+      },
+      backgroundImage: {
+        'orangeGradient': 'linear-gradient(to right, #f74c06, #f9bc2c)',
+      },
+
     },
   },
   plugins: [],
