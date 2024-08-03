daf866ae121704f2d63b5afab9be86b09974e90f
AtanasNikolov1 <atanasnik989@gmail.com>
2024-08-03 16:10:52 +0300

    Add wrapper component for LoginForm and SignUpWelcome

---
+++ src/components/features/auth/LoginContainer.tsx
@@ -0,0 +1,15 @@
+import LoginForm from "./LoginForm";
+import SignUpWelcome from "./SignUpWelcome";
+
+const LoginContainer = () => {
+  return (
+    <div className="flex items-center justify-center rounded-2xl bg-white">
+      <div className="flex w-full max-w-4xl overflow-hidden rounded-2xl border border-customOrange shadow-lg">
+        <LoginForm />
+        <SignUpWelcome />
+      </div>
+    </div>
+  );
+};
+
+export default LoginContainer;
