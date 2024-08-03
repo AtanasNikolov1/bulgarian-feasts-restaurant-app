d79ce0ed895e7b70856aa8568b446c473f9bc0e2
AtanasNikolov1 <atanasnik989@gmail.com>
2024-08-03 17:34:05 +0300

    Add dynamic signIn and signOut buttons

--- src/components/ui/userNav/UserNav.tsx
+++ src/components/ui/userNav/UserNav.tsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
 import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../../../services/authService";
+import { useAuth } from "../../../context/AuthContext";
 
-const userIcons = [
-  { icon: "cart-shopping", path: "/cart" },
-  { icon: "user", path: "/account" },
-];
+const userIcons = [{ icon: "cart-shopping", path: "/cart" }];
 
 const UserNav = () => {
+  const navigate = useNavigate();
+  const { user } = useAuth();
+
   const handleLogout = async () => {
     const result = await logout();
 
@@ -34,12 +34,21 @@ const UserNav = () => {
             </Link>
           </li>
         ))}
+        {!user && (
+          <li className="rounded-2xl p-2 text-3xl text-customOrange transition-all duration-450 ease-in-out hover:cursor-pointer hover:rounded-2xl hover:bg-customOrange hover:text-lightGray">
+            <Link to="login">
+              <i className={`fa-solid fa-user`}></i>
+            </Link>
+          </li>
+        )}
+        {user && (
           <button
             onClick={handleLogout}
             className="rounded-2xl p-2 text-3xl text-customOrange transition-all duration-450 ease-in-out hover:cursor-pointer hover:rounded-2xl hover:bg-customOrange hover:text-lightGray"
           >
             <i className="fa-solid fa-right-from-bracket"></i>
           </button>
+        )}
       </ul>
     </nav>
   );
