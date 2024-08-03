dcde59ea3ae457dc19bf0d40b73de09259895641
AtanasNikolov1 <atanasnik989@gmail.com>
2024-08-03 16:11:28 +0300

    Add the login page

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import LoginContainer from "../components/features/auth/LoginContainer";
+
+const LoginPage = () => {
+  return (
+    <div className="flex min-h-[90vh] items-center justify-center">
+      <LoginContainer />
+    </div>
+  );
+};
+
+export default LoginPage;
