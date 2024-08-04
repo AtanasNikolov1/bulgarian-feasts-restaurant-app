e049992338f6e2c4798968d89963c4aea8131ec7
AtanasNikolov1 <atanasnik989@gmail.com>
2024-08-04 23:47:47 +0300

    Add dynamic background color and logo

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -4,19 +4,25 @@ import useScrollPosition from "../../../hooks/useScrollPosition";
 import darkLogo from "../../../assets/images/dark-logo.png";
 import lightLogo from "../../../assets/images/light-logo.png";
 
-function Header() {
+function Header({ color }) {
   const scroll = useScrollPosition();
+
+  const headerStyle =
+    color === "transparent" && scroll
+      ? { backgroundColor: "snow" }
+      : { backgroundColor: color === "transparent" ? "transparent" : color };
 
   return (
     <header
-      className={`fixed top-0 flex w-full items-center justify-between ${scroll ? "bg-white shadow-md" : "bg-transparent"} z-10 px-9 py-1.5 transition-all duration-300`}
+      style={headerStyle}
+      className={`fixed top-0 z-10 flex w-full items-center justify-between px-9 py-1.5 shadow-md transition-all duration-300`}
     >
       <img
-        src={scroll ? darkLogo : lightLogo}
+        src={color === "transparent" && !scroll ? lightLogo : darkLogo}
         alt="logo"
         className="h-20 pl-10 pr-40"
       />
-      <MenuNav scroll={scroll} />
+      <MenuNav scroll={scroll} color={color} />
       <UserNav />
     </header>
   );
