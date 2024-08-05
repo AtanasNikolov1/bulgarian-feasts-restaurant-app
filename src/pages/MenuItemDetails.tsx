66499466d6c442ee68c3361c9e680e12c636616c
AtanasNikolov1 <atanasnik989@gmail.com>
2024-08-05 17:43:15 +0300

    Modify the MenuItemDetails page

--- src/pages/MenuItemDetails.tsx
+++ src/pages/MenuItemDetails.tsx
@@ -1,5 +1,13 @@
+import ItemDetails from "../components/features/details/ItemDetails";
+import Header from "../components/layout/header/Header";
+
 const MenuItemDetails = () => {
-  return <div>Menu Item Details</div>;
+  return (
+    <div className="mb-32 mt-44">
+      <Header color="snow" />
+      <ItemDetails />
+    </div>
+  );
 };
 
 export default MenuItemDetails;
