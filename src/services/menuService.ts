0bccab303433279214d305cab8441eae23afee59
AtanasNikolov1 <atanasnik989@gmail.com>
2024-08-05 17:19:53 +0300

    Add function to filter menu items in menu service

--- src/services/menuService.ts
+++ src/services/menuService.ts
@@ -17,3 +17,36 @@ export const fetchMenuItems = async () => {
   }
 };
 
+export const fetchFilteredMenuItems = async (category, price, rating) => {
+  let query = supabase.from("menu").select("*");
+
+  if (category && category.value !== null) {
+    query = query.eq("category", category.value);
+  }
+
+  if (price && price.value !== null) {
+    query = query.order("price", { ascending: price.value === "asc" });
+  }
+
+  if (rating && rating.value !== null) {
+    const ratingMin = rating.value;
+    const ratingMax = rating.value + 0.9;
+    query = query.gte("rating", ratingMin).lte("rating", ratingMax);
+  }
+
+  try {
+    const { data, error } = await query;
+
+    if (error) {
+      throw error;
+    }
+
+    return { data, error: null };
+  } catch (error) {
+    console.log(error.message);
+    return {
+      data: null,
+      error: "Failed to filter the menu items. Please try again later.",
+    };
+  }
+};
