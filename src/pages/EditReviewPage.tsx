b888464319e1338c72fef874c1533c5f335b7658
AtanasNikolov1 <atanasnik989@gmail.com>
2024-08-06 17:44:03 +0300

    Add the EditReviewPage

---
+++ src/pages/EditReviewPage.tsx
@@ -0,0 +1,13 @@
+import EditReviewForm from "../components/features/details/EditReviewForm";
+import Header from "../components/layout/header/Header";
+
+const EditReviewPage = () => {
+  return (
+    <div>
+      <Header color="snow" />
+      <EditReviewForm />
+    </div>
+  );
+};
+
+export default EditReviewPage;
