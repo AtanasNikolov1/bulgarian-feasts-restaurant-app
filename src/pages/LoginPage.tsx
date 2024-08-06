1194aa629e56df8c5bb4cf76582c35a270e76e6d
AtanasNikolov1 <atanasnik989@gmail.com>
2024-08-06 23:06:19 +0300

    Add navigation to login and register page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,10 @@
 import LoginContainer from "../components/features/auth/LoginContainer";
+import Header from "../components/layout/header/Header";
 
 const LoginPage = () => {
   return (
     <div className="flex min-h-[90vh] items-center justify-center">
+      <Header color="snow" />
       <LoginContainer />
     </div>
   );
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,8 +1,10 @@
 import RegisterContainer from "../components/features/auth/RegisterContainer";
+import Header from "../components/layout/header/Header";
 
 const RegisterPage = () => {
   return (
     <div className="flex min-h-[90vh] items-center justify-center">
+      <Header color="snow" />
       <RegisterContainer />
     </div>
   );
