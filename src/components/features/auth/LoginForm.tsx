637ae9e8218cabda67cd6613874421a8607d219f
AtanasNikolov1 <atanasnik989@gmail.com>
2024-08-06 23:04:25 +0300

    Add form validations on login form

--- src/components/features/auth/LoginForm.tsx
+++ src/components/features/auth/LoginForm.tsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { login } from "../../../services/authService";
 
 const LoginForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleLogin = async (data) => {
+    const { email, password } = data;
 
     const { success, message, user } = await login(email, password);
 
@@ -31,24 +35,41 @@ const LoginForm = () => {
   return (
     <form
       className="flex w-1/2 flex-col justify-center p-8 text-center"
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(handleLogin)}
     >
       <h2 className="mb-6 text-3xl font-semibold text-lightBlack">Sign in</h2>
+
       <input
         type="email"
         placeholder="Email"
-        className="mb-6 w-full rounded border border-gray-300 p-2 focus:border-customOrange focus:outline-none"
-        onChange={(e) => setEmail(e.target.value)}
+        className={`mt-4 w-full rounded border border-gray-300 p-2 focus:border-customOrange focus:outline-none ${errors.email ? "border-red-500" : ""}`}
+        {...register("email", {
+          required: "Email is required.",
+          pattern: {
+            value: /\S+@\S+\.\S+/,
+            message: "Email address is invalid.",
+          },
+        })}
       />
+      {errors.email && (
+        <p className="text-sm text-red-500">{errors.email.message}</p>
+      )}
+
       <input
         type="password"
         placeholder="Password"
-        className="mb-9 w-full rounded border border-gray-300 p-2 focus:border-customOrange focus:outline-none"
-        onChange={(e) => setPassword(e.target.value)}
+        className={`mt-4 w-full rounded border border-gray-300 p-2 focus:border-customOrange focus:outline-none ${errors.password ? "border-red-500" : ""}`}
+        {...register("password", {
+          required: "Password is required.",
+        })}
       />
+      {errors.password && (
+        <p className="text-sm text-red-500">{errors.password.message}</p>
+      )}
+
       <button
         type="submit"
-        className="w-full rounded-lg border-2 border-customOrange bg-customOrange py-2 font-medium text-white transition-colors duration-200 hover:bg-white hover:text-customOrange"
+        className="mt-9 w-full rounded-lg border-2 border-customOrange bg-customOrange py-2 font-medium text-white transition-colors duration-200 hover:bg-white hover:text-customOrange"
       >
         SIGN IN
       </button>
