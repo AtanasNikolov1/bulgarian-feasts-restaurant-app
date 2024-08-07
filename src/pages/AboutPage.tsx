3957eded12fa86f057aa4e92c510f01287a9d68f
AtanasNikolov1 <atanasnik989@gmail.com>
2024-08-07 15:53:03 +0300

    Create about page

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+import CulinaryExcellence from "../components/features/aboutUs/CulinaryExcellence";
+import Header from "../components/layout/header/Header";
+
+const AboutPage = () => {
+  return (
+    <>
+      <Header color="snow" />
+      <CulinaryExcellence />
+    </>
+  );
+};
+
+export default AboutPage;
