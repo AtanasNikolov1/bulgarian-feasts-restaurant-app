07b60e6fa6e91fead728c710d19e648299cb529c
AtanasNikolov1 <atanasnik989@gmail.com>
2024-08-07 17:08:05 +0300

    Create ContactUs page

---
+++ src/pages/ContactUs.tsx
@@ -0,0 +1,13 @@
+import ContactUsForm from "../components/features/contactUs/ContactUsForm";
+import Header from "../components/layout/header/Header";
+
+const ContactUs = () => {
+  return (
+    <>
+      <Header color="snow" />
+      <ContactUsForm />
+    </>
+  );
+};
+
+export default ContactUs;
